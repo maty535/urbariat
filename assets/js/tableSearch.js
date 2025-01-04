489dfdfa1e648acfa7da94b9abd96d2bd0531cfc
Matus Ivanecky <maty535@users.noreply.github.com>
2025-01-05 00:21:12 +0100

    Create tableSearch.js

---
+++ assets/js/tableSearch.js
@@ -0,0 +1,27 @@
+<script type="text/javascript">
+
+function search(){
+  
+  var s = document.getElementById('search').value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+  rows = (document.getElementsByTagName('tbody')[0]).getElementsByTagName('TR');
+  for(var i=0;i<rows.length;i++){
+    if(rows[i].textContent.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').indexOf(s)>0  || s.length==0 ) {
+	  rows[i].style.display ='';
+    } else {
+      rows[i].style.display ='none';
+    }
+  }
+}
+
+
+var timer;
+function delayedSearch() {
+	clearTimeout(timer);
+	console.log('delay-ing')
+    timer = setTimeout(function () {
+		console.log('delay-running')
+		search();
+    }, 500);
+  }
+</script>
