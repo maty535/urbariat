8444f94b4a5767738f990257862286e269d700f1
Matus Ivanecky <matus.ivanecky@gmail.com>
2025-12-26 22:44:36 +0100

    Fixe vypoctu dividendy 2024

--- assets/js/dividenda2024.js
+++ assets/js/dividenda2024.js
@@ -3,20 +3,19 @@
 function vypocetZiskuPodielnika(){
    
   // definicie ------------------------------------------------
-  zisk        = 20139.0 ;   // suma na finstate
+  zisk        = 20139.0 ;   // suma na finstate pre 2024
   urbarPlocha = 1286889.0 ; // plocha v m2   
   //-----------------------------------------------------------
    
    ziskNaRozdelenie = zisk * 0.9;     //  kolko z toho ide podielnikom, co si schvalili
    ucast     = parseFloat(document.getElementById('ucast-in').value);  // sila podielnika, de-facto Hlas v %
-   dividenda = (ucast/100.0)*ziskNaRozdelenie;
-   plocha    = (ucast/100)*urbarPlocha;
+   dividenda = (ucast*ziskNaRozdelenie)/100.0;
+   plocha    = (ucast*urbarPlocha)/100.0;
 
   // Zapíšeme výsledok do bunky v tfoot
   document.getElementById('ucast').textContent      = ucast.toFixed(4)     + " %";
   document.getElementById('plocha').textContent     = plocha.toFixed(4)    + " m2";
   document.getElementById('dividenda').textContent  = dividenda.toFixed(0) + " €";
-
 }
 
 var timer;
